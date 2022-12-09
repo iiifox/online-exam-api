928de46d45ac11890732212cc6fea2d563499fbf
iiifox <tianzhang.chn@gmail.com>
2022-12-09 23:07:25 +0800

    Knife4j集成SpringDoc-OpenApi

--- src/main/java/cn/edu/hutb/exam/OnlineExamApiApplication.java
+++ src/main/java/cn/edu/hutb/exam/OnlineExamApiApplication.java
@@ -1,13 +1,39 @@
 package cn.edu.hutb.exam;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
 @SpringBootApplication
 public class OnlineExamApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OnlineExamApiApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ApplicationContext application = SpringApplication.run(OnlineExamApiApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        // 未配置默认8080
+        if (port == null) {
+            port = "8080";
+        }
+        String path = env.getProperty("server.servlet.context-path");
+        // 未配置默认空白
+        if (path == null) {
+            path = "";
+        }
+
+        log.info("\n----------------------------------------------------------\n\t" +
+                "在线考试系统启动成功，访问路径如下:\n\t" +
+                "本地路径: \thttp://localhost:" + port + path + "/\n\t" +
+                "网络地址: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "API文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                "----------------------------------------------------------");
     }
 
 }
---
+++ src/main/java/cn/edu/hutb/exam/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package cn.edu.hutb.exam.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                // 在请求头当中配置JWT令牌字符串，SpringDoc需要额外在方法上添加注解，我们也可以在Knife4j中设置全局请求头
+                .components(new Components().addSecuritySchemes("token", new SecurityScheme()))
+                .info(new Info()
+                        .title("在线考试系统 API")
+                        .version("0.0.1")
+                        .contact(new Contact().name("田章").url("https://iiifox.github.io/").email("tianzhang.chn@gmail.com"))
+                        .description("使用Knife4j集成SpringDoc-OpenApi生成的后端API接口文档，" +
+                                "提供导出4种格式的离线文档(Html/Markdown/Word/OpenAPI)的功能。")
+                        .license(new License().name("Apache 2.0")));
+    }
+
+}

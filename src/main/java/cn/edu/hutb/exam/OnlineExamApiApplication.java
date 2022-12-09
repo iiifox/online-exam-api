3733d724be2f0b6927463aa1f6d3f0a817bcac3a
iiifox <tianzhang.chn@gmail.com>
2022-12-09 10:34:04 +0800

    Initial commit

---
+++ src/main/java/cn/edu/hutb/exam/OnlineExamApiApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.hutb.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExamApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/hutb/exam/OnlineExamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.hutb.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
